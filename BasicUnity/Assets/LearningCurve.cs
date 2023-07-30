e02428d5e32cbe5ad0a2d828bb2064d148d50bea
kimhyeok <kimmang940715@naver.com>
2023-07-30 22:11:33 +0900

    변수 에 대한 설명

--- BasicUnity/Assets/LearningCurve.cs
+++ BasicUnity/Assets/LearningCurve.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #region Variable
+    
+    // 1. 변수
+    // -개념적으로 변수는 물질계의 원자 처럼 프로그래밍의 가장 기본적인 단위다.
+    // 모든 것은 변수에서 시작하며, 변수 없이는 프로그램이 존재할 수 없다.
+    // -기술적으로 변수는 할당된 값을 보유하는 컴퓨터 메모리의 작은 부분이다. 모든 변수는 정보가
+    // 저장된 위치 (메모리 주소), 값, 타입(인스턴스, 숫자, 단어, 리스트 등)을 추적한다.
+    // -실제로 변수는 컨테이너다. 마음대로 변수를 새로 만들고, 채우고, 이동하고, 값을 변경하고
+    // 필요에 따라 참조할 수 있다. 심지어 변수는 비어 잇어도 여전히 유용하다.
+    
+    #endregion
+    
 }
