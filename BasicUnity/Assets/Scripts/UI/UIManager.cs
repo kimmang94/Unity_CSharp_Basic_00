3e8b90c5deaaf975a66900dc7f5607ce67622a11
kimhyeok <kimmang940715@naver.com>
2023-08-10 07:45:08 +0900

    옵션 패널 버튼기능 추가

--- BasicUnity/Assets/Scripts/UI/UIManager.cs
+++ BasicUnity/Assets/Scripts/UI/UIManager.cs
@@ -8,14 +8,22 @@ using Button = UnityEngine.UI.Button;
 
 public class UIManager : MonoBehaviour
 {
+    // IntroCanvas Btn
     [SerializeField] private Button startBtn = null;
     [SerializeField] private Button optionsBtn = null;
     [SerializeField] private Button quitBtn = null;
+    
+    [SerializeField] private GameObject optionPanel = null;
+    // panel Btn
+    [SerializeField] private Button agreeBtn = null;
+    [SerializeField] private Button cancleBtn = null;
     private void Awake()
     {
         startBtn.onClick.AddListener(OnClickGameStartBtn);
         optionsBtn.onClick.AddListener(OnClickOptionsBtn);
         quitBtn.onClick.AddListener(OnClickQuitBtn);        
+        agreeBtn.onClick.AddListener(OnAgreeBtn);
+        cancleBtn.onClick.AddListener(OnCancleBtn);
     }
 
     private void OnClickGameStartBtn()
@@ -25,6 +33,7 @@ public class UIManager : MonoBehaviour
     }
     private void OnClickOptionsBtn()
     {
+        optionPanel.SetActive(true);
         Debug.Log("Options");
     }
     
@@ -32,5 +41,15 @@ public class UIManager : MonoBehaviour
     {
         Debug.Log("App QUIT");
         Application.Quit();
+    }
+
+    private void OnCancleBtn()
+    {
+        optionPanel.SetActive(false);
+    }
+
+    private void OnAgreeBtn()
+    {
+        Debug.Log("옵션 데이터 저장");
     }
 }
