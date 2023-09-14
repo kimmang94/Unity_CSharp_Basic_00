5469073c1958cf40ef553a87034e05bfda5cae29
kimhyeok <kimmang940715@naver.com>
2023-09-15 02:31:24 +0900

    폴더 수정

--- BasicUnity/Assets/Scrpts/Managers/Managers.cs
+++ BasicUnity/Assets/Scrpts/Managers/Managers.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
