1a04680c40b7c4b6e86a117c05ecd82db505f83b
kimhyeok <kimmang940715@naver.com>
2023-09-17 20:29:07 +0900

    Local -> World 좌표계산

--- BasicUnity/Assets/Scrpts/PlayerController.cs
+++ BasicUnity/Assets/Scrpts/PlayerController.cs
@@ -16,22 +16,27 @@ public class PlayerController : MonoBehaviour
         // PlayerController
     private void Update()
     {
+        // Local -> World
+        // TransformDirection
+        
+        // World -> Loacl
+        // InversTransformDirection
         
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * (Time.deltaTime * _speed) ;
+            transform.position += transform.TransformDirection(Vector3.forward * (Time.deltaTime * _speed)) ;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= Vector3.back * (Time.deltaTime * _speed);
+            transform.position -= transform.TransformDirection(Vector3.back * (Time.deltaTime * _speed));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= Vector3.left * (Time.deltaTime * _speed);
+            transform.position -= transform.TransformDirection(Vector3.left * (Time.deltaTime * _speed));
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * (Time.deltaTime * _speed);
+            transform.position += transform.TransformDirection(Vector3.right * (Time.deltaTime * _speed));
         }
     }
 }
