3ec03cc8cf8480766bd283f6cbc78a7f7424ddd9
kimhyeok <kimmang940715@naver.com>
2023-09-18 04:45:08 +0900

    회전과 포지션 이동 추가

--- BasicUnity/Assets/Scrpts/PlayerController.cs
+++ BasicUnity/Assets/Scrpts/PlayerController.cs
@@ -10,26 +10,40 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-    
+
+    private float _yAngle = 0.0f;
     private void Update()
     {
+        _yAngle += Time.deltaTime * _speed;
+        
+        // 절대 회전 값
+        //transform.eulerAngles = new Vector3(0f, _yAngle, 0f);
+        
+        // +- delta
+        //transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100f , 0.0f));
 
+        //transform.rotation = Quaternion.Euler(new Vector3(0f, _yAngle, 0f));
+        //Quaternion qt = transform.rotation;
         
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * (Time.deltaTime * _speed));
+           transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.3f);
+           transform.position += (Vector3.forward * (Time.deltaTime * _speed));
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate((Vector3.back * (Time.deltaTime * _speed)));
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.3f);
+            transform.position += ((Vector3.back * (Time.deltaTime * _speed)));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate((Vector3.left * (Time.deltaTime * _speed)));
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.3f);
+            transform.position += ((Vector3.left * (Time.deltaTime * _speed)));
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate((Vector3.right * (Time.deltaTime * _speed)));
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.3f);
+            transform.position += ((Vector3.right * (Time.deltaTime * _speed)));
         }
     }
 }
