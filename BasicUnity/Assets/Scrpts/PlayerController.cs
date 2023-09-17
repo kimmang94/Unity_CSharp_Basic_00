b5968ceb39383c8854d8e366d2b6753873c0e5e5
kimhyeok <kimmang940715@naver.com>
2023-09-17 20:07:47 +0900

    transform.position 에 값을 더해 플레이어 움직이는 코드

--- BasicUnity/Assets/Scrpts/PlayerController.cs
+++ BasicUnity/Assets/Scrpts/PlayerController.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // GameObject(Player)
+        // Transform
+        // PlayerController
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0f, 0f, 1f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0f, 0f, 1f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1f, 0f, 0f);
+        }
     }
 }
