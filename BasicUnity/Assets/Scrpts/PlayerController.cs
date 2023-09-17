aa0124f8416868bb53e2d5dbad2c5d2e57e328ca
kimhyeok <kimmang940715@naver.com>
2023-09-18 04:22:28 +0900

    Rotation 을 알아보기 위한 코드정리

--- BasicUnity/Assets/Scrpts/PlayerController.cs
+++ BasicUnity/Assets/Scrpts/PlayerController.cs
@@ -2,81 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 1.위치 벡터
-// 2.방향 벡터
-struct MyVector
-{
-    public float x;
-    public float y;
-    public float z;
-
-    // 피타 고라스 정리
-    //          +
-    //     +    +
-    // +--------+
-    public float magnitude
-    {
-        get { return Mathf.Sqrt(x * x + y * y + z * z) ; }
-    }
-
-    public MyVector normalized
-    {
-        get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); }
-    }
-    public MyVector(float x, float y, float z)
-    {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public static MyVector operator +(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
-    }
-    
-    public static MyVector operator -(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
-    }
-    
-    public static MyVector operator *(MyVector a, float d)
-    {
-        return new MyVector(a.x * d, a.y *d, a.z * d);
-    }
-}
-
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 10.0f ;
     private void Start()
     {
-        MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
-        MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
-        MyVector dir = to - from; // ( 5.0f, 0.0f, 0.0f) 방향벡터
-
-        dir = dir.normalized; // (1.0f, 0.0f, 0.0f)
-
-        MyVector newPos = from + dir * _speed;
-
-        // 방향 벡터
-        // 1. 거리 (크기) magnitude
-        // 2. 실제 방향
+        
     }
-
-    // GameObject(Player)
-        // Transform
-        // PlayerController
+    
     private void Update()
     {
-        // Local -> World
-        // TransformDirection
-        
-        // World -> Loacl
-        // InversTransformDirection
 
-        
         
         if (Input.GetKey(KeyCode.W))
         {
