28f2c6d7cef24f0bb0baa98093431bfedf5b4803
kimhyeok <kimmang940715@naver.com>
2023-09-17 20:14:34 +0900

    speed 변수 추가

--- BasicUnity/Assets/Scrpts/PlayerController.cs
+++ BasicUnity/Assets/Scrpts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 10.0f ;
     private void Start()
     {
         
@@ -15,21 +16,22 @@ public class PlayerController : MonoBehaviour
         // PlayerController
     private void Update()
     {
+        
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0f, 0f, 1f);
+            transform.position += new Vector3(0f, 0f, 1f) * (Time.deltaTime * _speed) ;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= new Vector3(0f, 0f, 1f);
+            transform.position -= new Vector3(0f, 0f, 1f) * (Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(1f, 0f, 0f);
+            transform.position -= new Vector3(1f, 0f, 0f) * (Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(1f, 0f, 0f);
+            transform.position += new Vector3(1f, 0f, 0f) * (Time.deltaTime * _speed);
         }
     }
 }
