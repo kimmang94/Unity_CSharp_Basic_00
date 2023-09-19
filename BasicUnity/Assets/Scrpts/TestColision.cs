40d82b2c633bba0fb7c64236bbf2f7b101ed90df
kimhyeok <kimmang940715@naver.com>
2023-09-19 12:45:30 +0900

    OnCollision 발생 조건 추가

--- BasicUnity/Assets/Scrpts/TestColision.cs
+++ BasicUnity/Assets/Scrpts/TestColision.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class TestColision : MonoBehaviour
 {
+    // 1. 나한테 RigidBody 가 있어야 한다 is Kinematic : Off
+    // 2. 나한테 Colider가 있어야 한다 isTrigger : off
+    // 3. 상태한테 Colider 가 있어야 한다 isTrigger : off
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log("Collision!");
