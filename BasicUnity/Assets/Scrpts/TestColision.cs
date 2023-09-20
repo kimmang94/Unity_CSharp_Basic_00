03d5396ebd1f35c81a3714f32c13537becd6ce94
kimhyeok <kimmang940715@naver.com>
2023-09-21 03:54:39 +0900

    LayerMask 에대한 내용

--- BasicUnity/Assets/Scrpts/TestColision.cs
+++ BasicUnity/Assets/Scrpts/TestColision.cs
@@ -30,6 +30,26 @@ public class TestColision : MonoBehaviour
     {
         // Local <-> World <-> Viewport <-> Screen (화면)
         //Debug.Log(Input.mousePosition); // Screen
-        Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // ViewPort
+        //Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // ViewPort
+
+        #region World
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+            int mask = ((1 << 8) | (1 << 9));
+            
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, mask))
+            {
+                Debug.Log($"RayCast Camera @ {hit.collider.gameObject.name}");
+            }
+        }
+        
+
+        #endregion
+       
     }
 }
