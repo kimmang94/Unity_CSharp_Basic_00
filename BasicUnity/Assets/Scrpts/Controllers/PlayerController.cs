930a0b9ad8aa13b61e8763ea1789213682b5951a
kimhyeok <kimmang940715@naver.com>
2023-09-23 20:34:23 +0900

    마우스클릭 추가

--- BasicUnity/Assets/Scrpts/Controllers/PlayerController.cs
+++ BasicUnity/Assets/Scrpts/Controllers/PlayerController.cs
@@ -1,17 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed = 10.0f ;
+    [SerializeField] private float _speed = 10.0f;
+
+    private bool _moveToDest = false;
+    private Vector3 _destPos;
+    private float dirMinLength = 0.0001f;
     private void Start()
     {
         Managers.Input.KeyAction -= OnKeyboard;
         Managers.Input.KeyAction += OnKeyboard;
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
     }
 
+    private void Update()
+    {
+        if (_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position;
+            
+            if (dir.magnitude < dirMinLength)
+            {
+                _moveToDest = false;
+            }
+            else
+            {
+                transform.position += dir.normalized * _speed * Time.deltaTime;
+                transform.LookAt(_destPos);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 키보드 이동
+    /// </summary>
     private void OnKeyboard()
     {
         if (Input.GetKey(KeyCode.W))
@@ -19,20 +46,46 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.3f);
             transform.position += (Vector3.forward * (Time.deltaTime * _speed));
         }
+
         if (Input.GetKey(KeyCode.S))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.3f);
             transform.position += ((Vector3.back * (Time.deltaTime * _speed)));
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.3f);
             transform.position += ((Vector3.left * (Time.deltaTime * _speed)));
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.3f);
             transform.position += ((Vector3.right * (Time.deltaTime * _speed)));
         }
+
+        _moveToDest = false;
+    }
+    /// <summary>
+    /// 마우스 이동
+    /// </summary>
+    private void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (evt != Define.MouseEvent.Click)
+        {
+            return;
+        }
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+        Debug.Log("Click");
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        { 
+            _destPos = hit.point; 
+            _moveToDest = true;
+        }
     }
 }
+
