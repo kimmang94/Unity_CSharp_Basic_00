b55931a139e1f721f533b5eb5eaedb847a643ef7
kimhyeok <kimmang940715@naver.com>
2023-09-29 20:45:46 +0900

    UIBase 스크립트 생성후 이동

--- BasicUnity/Assets/Scrpts/UI/UI_Button.cs
+++ BasicUnity/Assets/Scrpts/UI/UI_Button.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
     private int score = 0;
-    private Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, Object[]>();
-    
+ 
     private enum Buttons
     {
         PointButton,
@@ -21,39 +20,23 @@ public class UI_Button : MonoBehaviour
         ScoreText,
         
     }
+
+    private enum GameObject
+    {
+        TestObject,
+        
+    }
     
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
+        Bind<UnityEngine.GameObject>(typeof(GameObject));
 
-        Get<Text>((int)Texts.ScoreText).text = "Bind Test";
+        GetText((int)Texts.ScoreText).text = "Bind Test";
     }
 
-    private void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
-
-        for (int i = 0; i < names.Length; i++)
-        {
-            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-        }
-    }
-
-    private T Get<T>(int index) where T : UnityEngine.Object
-    {
-        UnityEngine.Object[] objects = null;
-        if (_objects.TryGetValue(typeof(T), out objects) == false)
-        {
-            return null;
-        }
-
-        return objects[index] as T;
-    }
-    
+   
     public void OnButtonClicked()
     {
         score++;
--- BasicUnity/Assets/Scrpts/Utils/Util.cs
+++ BasicUnity/Assets/Scrpts/Utils/Util.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Util
 {
+    public static GameObject FindChild(GameObject go, string name = null, bool recursive = false)
+    {
+        Transform transform = FindChild<Transform>(go, name, recursive);
+        if (transform == null)
+            return null;
+
+        return transform.gameObject;
+    }
+
     public static T FindChild<T>(GameObject go, string name = null, bool recursive = false) where T : UnityEngine.Object
     {
         if (go == null)
-        {
             return null;
-        }
 
         if (recursive == false)
         {
@@ -20,21 +28,16 @@ public class Util
                 {
                     T component = transform.GetComponent<T>();
                     if (component != null)
-                    {
                         return component;
-                    }
                 }
             }
-
         }
         else
         {
             foreach (T component in go.GetComponentsInChildren<T>())
             {
                 if (string.IsNullOrEmpty(name) || component.name == name)
-                {
                     return component;
-                }
             }
         }
 
