600af7e97e868283acd4d3d6442f4f93ffac648d
kimhyeok <kimmang940715@naver.com>
2023-09-29 17:55:06 +0900

    UI Button 수정

--- BasicUnity/Assets/Scrpts/UI/UI_Button.cs
+++ BasicUnity/Assets/Scrpts/UI/UI_Button.cs
@@ -1,17 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 public class UI_Button : MonoBehaviour
 {
-
-    [SerializeField] private Text _text;
-
     private int score = 0;
+    private Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, Object[]>();
+    
+    private enum Buttons
+    {
+        PointButton,
+        
+    }
+    private enum Texts
+    {
+        PointText,
+        ScoreText,
+        
+    }
+    
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+    }
+
+    private void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+        }
+    }
+    
     public void OnButtonClicked()
     {
         score++;
-        _text.text = $"점수 : {score} 점";
     }
 }
