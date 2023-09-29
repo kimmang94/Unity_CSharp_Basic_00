8624960f135a757a2b8018cdb8c791c1bd47449d
kimhyeok <kimmang940715@naver.com>
2023-09-29 21:06:08 +0900

    UIEventHandler 생성

--- BasicUnity/Assets/Scrpts/UI/UI_Button.cs
+++ BasicUnity/Assets/Scrpts/UI/UI_Button.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
@@ -26,14 +27,23 @@ public class UI_Button : UI_Base
         TestObject,
         
     }
+
+    private enum Images
+    { 
+        ItemIcon,
+    }
     
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
         Bind<UnityEngine.GameObject>(typeof(GameObject));
-
+        Bind<Image>(typeof(Images));
+        
         GetText((int)Texts.ScoreText).text = "Bind Test";
+        UnityEngine.GameObject go = GetImage((int)Images.ItemIcon).gameObject;
+        UI_EvnetHandler evt = go.GetComponent<UI_EvnetHandler>();
+        evt.OnDragHandler += ((PointerEventData data) => { evt.gameObject.transform.position = data.position; });
     }
 
    
--- BasicUnity/Assets/Scrpts/UI/UI_EvnetHandler.cs
+++ BasicUnity/Assets/Scrpts/UI/UI_EvnetHandler.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class UI_EvnetHandler : MonoBehaviour
+public class UI_EvnetHandler : MonoBehaviour, IBeginDragHandler, IDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public Action<PointerEventData> OnBeginDragHandler = null;
+    public Action<PointerEventData> OnDragHandler = null;
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        if (OnBeginDragHandler != null)
+        {
+            OnBeginDragHandler.Invoke(eventData);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
         
+        if (OnDragHandler != null)
+        {
+            OnDragHandler.Invoke(eventData);
+        }
     }
 }
