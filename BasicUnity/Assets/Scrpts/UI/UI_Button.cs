5f7700ff6be792e9b5b8f3139eb3fa4d9d93986d
kimhyeok <kimmang940715@naver.com>
2023-09-29 15:26:46 +0900

    버튼 클릭시 점수 추가

--- BasicUnity/Assets/Scrpts/UI/UI_Button.cs
+++ BasicUnity/Assets/Scrpts/UI/UI_Button.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Button : MonoBehaviour
 {
+
+    [SerializeField] private Text _text;
+
+    private int score = 0;
     public void OnButtonClicked()
     {
-        Debug.Log("Clicked!");
+        score++;
+        _text.text = $"점수 : {score} 점";
     }
 }
