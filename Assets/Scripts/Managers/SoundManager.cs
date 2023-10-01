9c82f7c7ad00084fa3b3c0b25c4a65316970dbe1
kimhyeok <kimmang940715@naver.com>
2023-10-02 02:11:18 +0900

    사운드 매니저 스크립트 추가

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager 
+{
+    // MP3 Player -> AudioSource
+    // MP3 음원 -> Audio Clip
+    // 관객(귀) -> AudioListener
+}
