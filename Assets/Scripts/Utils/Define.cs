f9a3f88076a377f6eece34a205aef3629884f965
kimhyeok <kimmang940715@naver.com>
2023-10-02 00:55:39 +0900

    씬매니저를 위한 스크립트 추가

--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Define
 {
+    public enum Scene
+    {
+        UnKnown,
+        Login,
+        Lobby,
+        Game,
+    }
     public enum UIEvent
     {
         Click,
