29e0ba766001eb89855a2bbdad90c8117e2f7dec
kimhyeok <kimmang940715@naver.com>
2023-10-02 04:20:14 +0900

    사운드 테스트 스크립트 및 사운드 위치변경

---
+++ Assets/TestSound.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public AudioClip audioClip;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Managers.Sound.Play( "UnityChan/univ0001",Define.Sound.Effect);
+            Debug.Log("Hit");
+        }
+      
+    }
+}
