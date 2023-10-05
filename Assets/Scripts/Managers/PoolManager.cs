a5fe76109a272a367e380d176359321e880d1b05
kimhyeok <kimmang940715@naver.com>
2023-10-06 03:30:32 +0900

    풀매니저 구현 1

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -4,5 +4,98 @@ using UnityEngine;
 
 public class PoolManager
 {
+    #region Pool
+
+    class Pool
+    {
+        public GameObject Original { get; private set; }
+        public Transform Root { get; set; }
+
+        private Stack<Poolable> _poolStack = new Stack<Poolable>();
+
+        public void Init(GameObject original, int count = 5)
+        {
+            Original = original;
+            Root = new GameObject().transform;
+            Root.name = $"{original.name}_Root";
+
+            for (int i = 0; i < count; i++)
+            {
+                Push(Create());
+            }
+            
+        }
+
+        Poolable Create()
+        {
+            GameObject go = Object.Instantiate<GameObject>(Original);
+            go.name = Original.name;
+            return go.GetOrAddComponent<Poolable>();
+        }
+
+        public void Push(Poolable poolable)
+        {
+            if (poolable == null)
+            {
+                return;
+            }
+
+            poolable.transform.parent = Root;
+            poolable.gameObject.SetActive(false);
+            poolable.IsUsing = false;
+
+            _poolStack.Push(poolable);
+        }
+
+        public Poolable Pop(Transform parent)
+        {
+            Poolable poolable;
+            if (_poolStack.Count > 0)
+            {
+                poolable = _poolStack.Pop();
+            }
+            else
+            {
+                poolable = Create();
+            }
+            poolable.gameObject.SetActive(true);
+            poolable.transform.parent = parent;
+            poolable.IsUsing = true;
+
+            return poolable;
+        }
+    }
+
+
+    #endregion
     
+    private Dictionary<string, Pool> _pool = new Dictionary<string, Pool>();
+    private Transform _root;
+    
+    public void Init()
+    {
+        if (_root = null)
+        {
+            _root = new GameObject { name = "Pool_Root" }.transform;
+            Object.DontDestroyOnLoad(_root);
+        }
+    }
+
+    public void Push(Poolable poolable)
+    {
+        
+    }
+
+    public Poolable Pop(GameObject original, Transform parent = null)
+    {
+        
+        return null;
+    }
+
+    public GameObject GetOriginal(string name)
+    {
+        return null;
+    }
+
+
 }
--- Assets/Scripts/Managers/Poolable.cs
+++ Assets/Scripts/Managers/Poolable.cs
@@ -6,4 +6,5 @@ public class Poolable : MonoBehaviour
 {
     // 해당 스크립트를 갖고있으면 pooling
     // 해당 스크립트가 없다면 none
+    public bool IsUsing;
 }
